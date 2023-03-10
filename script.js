f1991ac19eba6c3f6383fb388ebc72ebd7f4d29c
Devair Martins <devair.martins@gmail.com>
2023-03-10 10:10:25 -0300

    correções

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ var prompt = require('prompt-sync')();
 
 let lista = [];
 
+var aux = prompt('Digite a propriedade que você deseja adicionar a lista ou escreva "sair" para apresentar a lista. ');
+
 do {
-  var aux = prompt('Digite a propriedade que você deseja adicionar a lista ou escreva "sair" para apresentar a lista.');
 
   if (aux == 'sair') {
     console.log('Saindo da aplicação.')
@@ -16,12 +17,12 @@ do {
     console.log('Propiedade adicionada.');
     do {
       console.log(lista);
-      aux = prompt('Digite a propriedade que você deseja adicionar a lista ou escreva "sair" para apresentar a lista.');
+      aux = prompt('Digite a propriedade que você deseja adicionar a lista ou escreva "sair" para apresentar a lista. ');
       console.log(aux);
       if (aux == 'sair') {
-        console.log('saindo')
+        console.log('Saindo da aplicação')
       } else if (aux == null || aux == '' || aux == ' ') {
-        console.log('opção inválida')
+        console.log('Opção inválida')
       } else {
         lista.push(aux);
         console.log(lista);
